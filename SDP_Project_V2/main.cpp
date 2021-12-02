d737c4d04678cee4c852d291416902c404a2086d
Tyler Li <tydudebtd5@gmail.com>
2021-12-02 14:12:52 -0500

    back button works baby

--- SDP_Project_V2/main.cpp
+++ SDP_Project_V2/main.cpp
@@ -10,6 +10,7 @@ class Menu
         void stats(); //Stats function
         void rules(); //Instructions function
         void credits(); //Credits function
+        void back(); //Back button function
 };
 
 /*Main function*/
@@ -21,48 +22,13 @@ game.ss(); //Load the start screen
 if(LCD.Touch(&x,&y)){ //Enter menu if start screen is touched
     game.menu();
 }
-
 //Clicking the instructions button;
-while(true)
-    {
-        /* Clear the touch buffer so touches made before
-           this call are discarded */
-        LCD.ClearBuffer();
-        /* Wait until the user touches the screen */
-        while(!LCD.Touch(&x,&y)) {};
-        /* Wait until the touch releases */
-        while(LCD.Touch(&x,&y)) {};
-        if((x > 50 && x < 220) && (y > 20 && y < 60))
-        {
-            LCD.Clear();
-            LCD.Update();
-            game.game();
-        }
-        else if((x > 50 && x < 220) && (y > 70 && y < 110))
-        {
-            LCD.Clear();
-            LCD.Update();
-            game.stats();
-        }
-        else if((x > 50 && x < 220) && (y > 120 && y < 160))
-        {
-            LCD.Clear();
-            LCD.Update();
-            game.rules();
-        }
-        else if((x > 50 && x < 220) && (y > 170 && y < 210))
-        {
-            LCD.Clear();
-            LCD.Update();
-            game.credits();
-        }
-    }
-
-
 }
+
 /*After int main*/
 void Menu::menu() //Menu function definition 
 {
+    float x, y;
     // Clear background
     //Set background to green
     LCD.SetBackgroundColor(GREEN);
@@ -79,6 +45,47 @@ void Menu::menu() //Menu function definition
     LCD.WriteAt("Instructions", 90, 133);
     LCD.WriteAt("Credits", 90, 183);
     LCD.Update();
+    Sleep(1);
+    LCD.ClearBuffer();
+    while(true)
+    {
+        /* Clear the touch buffer so touches made before
+           this call are discarded */
+        LCD.ClearBuffer();
+        /* Wait until the user touches the screen */
+        while(!LCD.Touch(&x,&y)) {};
+        /* Wait until the touch releases */
+        while(LCD.Touch(&x,&y)) {};
+        if((x > 50 && x < 270) && (y > 20 && y < 60))
+        {
+            LCD.Clear();
+            LCD.Update();
+            (*this).game();
+            break;
+        }
+        else if((x > 50 && x < 270) && (y > 70 && y < 110))
+        {
+            LCD.Clear();
+            LCD.Update();
+            (*this).stats();
+            break;
+        }
+        else if((x > 50 && x < 270) && (y > 120 && y < 160))
+        {
+            LCD.Clear();
+            LCD.Update();
+            (*this).rules();
+            break;
+        }
+        else if((x > 50 && x < 270) && (y > 170 && y < 210))
+        {
+            LCD.Clear();
+            LCD.Update();
+            (*this).credits();
+            break;
+        }
+        LCD.Update();
+    }
 }
 void Menu::ss()
 {
@@ -96,6 +103,7 @@ void Menu::ss()
 
 void Menu::game()
 {
+    float x, y;
 LCD.SetBackgroundColor(GREEN);
     // Clear background
     //Set background to green
@@ -109,31 +117,57 @@ LCD.SetBackgroundColor(GREEN);
     LCD.DrawRectangle(50,150,220,40);
     LCD.WriteAt("Easy", 135, 100);
     LCD.WriteAt("Hard", 135, 160);
-    LCD.Update();
+    back();
 }
 
 void Menu::stats()
 {
+float x, y;
 LCD.WriteLine("Status Report: You suck at this game");
 LCD.SetBackgroundColor(GREEN);
+back();
 }
 
 void Menu::rules()
 {
+float x, y;
 LCD.WriteLine("Shoot the shapes with the cannon. It's not that hard");
-// LCD.WriteLine("This is the cannon");
-// LCD.DrawCircle(125, 195, 35);
-// LCD.DrawCircle(195, 195, 35);
-// LCD.DrawCircle(160, 55, 35);
-// LCD.DrawVerticalLine(125,55,160);
-// LCD.DrawVerticalLine(195,55,160);
-// LCD.DrawVerticalLine(160,20,25);
 LCD.SetBackgroundColor(GREEN); 
+back();
 }
 
 void Menu::credits()
 {
 LCD.WriteLine("The amazing Tyler Li and  Ben Lerner");
 LCD.SetBackgroundColor(GREEN);
+back();
 }
 
+//Back button definiton
+void Menu::back()
+{
+float x, y;
+LCD.DrawRectangle(250, 200, 60, 20);
+    LCD.WriteAt("Back",250, 205);
+    while(!LCD.Touch(&x,&y))
+    {
+       LCD.Update(); 
+    }
+    while(true)
+    {
+        /* Clear the touch buffer so touches made before
+           this call are discarded */
+        LCD.Clear();
+        LCD.ClearBuffer();
+        /* Wait until the user touches the screen */
+        while(!LCD.Touch(&x,&y)) {};
+        /* Wait until the touch releases */
+        while(LCD.Touch(&x,&y)) {};
+        if((x > 250 && x < 310) && (y > 200 && y < 220))
+        {
+            LCD.Clear();
+            LCD.Update();
+            (*this).menu();
+        }
+    }
+}
